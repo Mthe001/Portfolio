2292e3b3884a2d30c86b7080abaffa0015d3100a
MThe001 <mdredwanulhaque01614591672@gmail.com>
2025-02-07 19:06:05 +0600

    remove-scroll-proggress

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,6 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/ui/Footer";
-import { ScrollProgress } from "@/components/ui/scroll-progress";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,7 +31,7 @@ export default function RootLayout({ children }) {
          enableSystem
          disableTransitionOnChange
         >
-          <ScrollProgress />
+          
           <Navbar/>
           {children}
           <Footer/>
